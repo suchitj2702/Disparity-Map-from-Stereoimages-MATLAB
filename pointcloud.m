b211d3bf99d0ca26335ed4c1abed28bf1433973d
Suchit Jain <sj535@snu.edu.in>
2018-10-18 21:06:19 +0530

    basic_code

---
+++ pointcloud.m
@@ -0,0 +1,30 @@
+img = imread('depthsense6.png');
+
+left = img(1:1242, 1:2208, :);
+right = img(1:1242, 2209:4416, :);
+
+%left = imread('left.png');
+%right = imread('right.png');
+
+[LeftRect, RightRect] = rectifyStereoImages(left, right, stereoParams, 'OutputView', 'valid');
+
+figure;
+imtool(stereoAnaglyph(LeftRect, RightRect));
+
+leftgray = rgb2gray(LeftRect);
+rightgray = rgb2gray(RightRect);
+disparitymap = disparity(leftgray, rightgray, 'BlockSize', 59);
+%disparitymap = medfilt2(disparitymap);
+
+figure;
+imshow(disparitymap, [0, 64]);
+title('disparity');
+colormap jet
+colorbar
+
+points3D = reconstructScene(disparitymap, stereoParams);
+
+%points3D = points3D./1000;
+ptCloud = pointCloud(points3D, 'Color', LeftRect);
+
+pcwrite(ptCloud, 'pointcloud2');
